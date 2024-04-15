a8c9e0b62e45dda9201e2110229f22de5eb7ae04
def-life <abhishek.rana.7174@gmail.com>
2024-04-15 18:55:02 +0530

    add some space

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -4,6 +4,7 @@ import { authOptions } from "./lib/auth";
 import Link from "next/link";
 
 export default async function Page() {
+
   const session = await getServerSession(authOptions);
   if (session?.user) {
     redirect('/dashboard')
